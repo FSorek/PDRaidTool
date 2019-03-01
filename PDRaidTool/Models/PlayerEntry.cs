a6981febec8d84f186ffa22c5654873bc182e24e
fsorek <filip.sorek96@gmail.com>
2019-03-01 20:43:57 +0100

    Created model templates and initial project hierarchy

---
+++ PDRaidTool/Models/PlayerEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDRaidTool.Models
+{
+    public class PlayerEntry
+    {
+        public int Id;
+        public Profession Profession;
+        public Specialisation Specialisation;
+        public Role Role;
+        public byte Preference;
+    }
+}
