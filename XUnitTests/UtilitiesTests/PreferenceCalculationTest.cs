88ec63ccb93dd3c516bfaed3abdda9735543b2a9
fsorek <filip.sorek96@gmail.com>
2019-04-19 11:25:17 +0200

    small test refactor

--- XUnitTests/UtilitiesTests/PreferenceCalculationTest.cs
+++ XUnitTests/UtilitiesTests/PreferenceCalculationTest.cs
@@ -10,22 +10,16 @@ namespace XUnitTests.UtilitiesTests
 {
     public class PreferenceCalculationTest
     {
-        RaidSlot[] slots = new RaidSlot[]
-        {
-            new RaidSlot(){Id = 0, ProfessionId = 1, RoleId = 1, SpecialisationId = 1},
-            new RaidSlot(){Id = 1, ProfessionId = 3, RoleId = 2, SpecialisationId = 5}
-        };
         Player[] players = new Player[]
         {
             new Player(){Id = 0, Nickname = "okito"}, new Player(){Id = 1, Nickname = "vi"}
         };
-
-        RaidSlot[] slots2 = new RaidSlot[]
+        RaidSlot[] slots = new RaidSlot[]
         {
             new RaidSlot(){Id = 0, ProfessionId = 1, RoleId = 3, SpecialisationId = 3},
             new RaidSlot(){Id = 1, ProfessionId = 0, RoleId = 1, SpecialisationId = 0}
         };
-        PlayerEntry[] entries2 = new PlayerEntry[]
+        PlayerEntry[] entries = new PlayerEntry[]
         {
             new PlayerEntry(){Id = 0, PlayerId = 2, Preference = 7, ProfessionId = 2, RoleId = 2, SpecialisationId = 3},
             new PlayerEntry(){Id = 1, PlayerId = 0, Preference = 5, ProfessionId = 2, RoleId = 1, SpecialisationId = 6},
@@ -98,7 +92,7 @@ namespace XUnitTests.UtilitiesTests
         [Fact]
         public void FirstIdIsOne()
         {
-            var matrix = preferenceCalculationClass.CreateEntryMatrix(slots2, players, entries2);
+            var matrix = preferenceCalculationClass.CreateEntryMatrix(slots, players, entries);
             var test = HungarianAlgorithm.FindAssignments(matrix);
             Assert.Equal(1,test[0]);
         }
@@ -106,7 +100,7 @@ namespace XUnitTests.UtilitiesTests
         [Fact]
         public void SecondIdIsFive()
         {
-            var matrix = preferenceCalculationClass.CreateEntryMatrix(slots2, players, entries2);
+            var matrix = preferenceCalculationClass.CreateEntryMatrix(slots, players, entries);
             var test = HungarianAlgorithm.FindAssignments(matrix);
             Assert.Equal(5, test[1]);
         }
@@ -114,16 +108,6 @@ namespace XUnitTests.UtilitiesTests
         [Fact]
         public void FindHighestPreferenceIdFoundId()
         {
-            PlayerEntry[] entries = new PlayerEntry[]
-            {
-                new PlayerEntry(){Id = 0, PlayerId = 1, Preference = 3, ProfessionId = 1, RoleId = 1, SpecialisationId = 1},
-                new PlayerEntry(){Id = 1, PlayerId = 1, Preference = 4, ProfessionId = 1, RoleId = 1, SpecialisationId = 1},
-                new PlayerEntry(){Id = 2, PlayerId = 1, Preference = 5, ProfessionId = 1, RoleId = 1, SpecialisationId = 1},
-                new PlayerEntry(){Id = 3, PlayerId = 1, Preference = 6, ProfessionId = 1, RoleId = 1, SpecialisationId = 1},
-                new PlayerEntry(){Id = 4, PlayerId = 1, Preference = 7, ProfessionId = 1, RoleId = 1, SpecialisationId = 1},
-                new PlayerEntry(){Id = 5, PlayerId = 1, Preference = 8, ProfessionId = 1, RoleId = 1, SpecialisationId = 1},
-                new PlayerEntry(){Id = 6, PlayerId = 1, Preference = 10, ProfessionId = 3, RoleId = 3, SpecialisationId = 3},
-            };
             var test = preferenceCalculationClass.FindHighestPreferenceId(slots[0], entries);
             Assert.Equal(5, test);
         }
